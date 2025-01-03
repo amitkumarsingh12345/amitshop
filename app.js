a052013470a6c8c30eadd79cd3902b662f4bf76b
omware12345 <omwareinfosystem@gmail.com>
2025-01-02 22:59:28 -0800

    xx

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'ui/public')));
-
-// Serve React frontend
+app.use(express.static(path.join(__dirname, '/ui/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'ui/public', 'index.html'));
+    res.sendFile(path.join(__dirname + '/ui/build/index.html'));
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 1000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
